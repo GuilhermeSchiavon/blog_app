ad84021ecd336653e86eff98a68f0279c35d5e2d
Guilherme Américo Schiavon <guilherme.schiavon@outlook.com>
2022-05-06 01:38:13 -0300

    Listar Categorias e Editar

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const express = require("express")
+//const { render } = require("express/lib/response")
 const router = express.Router()
 const mongoose = require("mongoose")
 require("../models/Categoria")
@@ -9,7 +10,12 @@ router.get('/', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-   res.render("admin/categorias")
+   Categoria.find().sort({date: 'DESC'}).then((categorias) => {
+      res.render("admin/categorias", {categorias: categorias})
+   }).catch((err) => {
+      req.flash("error_msg", "Houve um erro ao carregar as Categorias")
+      res.redirect("/admim")
+   })
 })
 
 router.get('/categorias/add', (req, res) => {
@@ -49,6 +55,34 @@ router.post('/categorias/nova', (req, res) => {
       })
    }
 
+})
+
+router.get('/categorias/edit/:id', (req, res) => {
+   Categoria.findOne({_id: req.params.id}).then((categoria) => {
+      res.render("admin/edit_categorias", {categoria: categoria})
+   }).catch((err) => {
+      req.flash("error_msg", "Houve um erro, Categoria não existente")
+      res.redirect("/admin/categorias")
+   })
+})
+
+router.post('/categorias/edit', (req, res) => {
+   Categoria.findOne({_id: req.body._id}).then((categoria) => {
+      categoria.nome = req.body.nome
+      categoria.slug = req.body.slug
+
+      categoria.save().then(() => {
+         req.flash("success_msg", "Categoria editada com Sucesso")
+         res.redirect("/admin/categorias")
+      }).catch((err) => {
+         req.flash("error_msg", "Houve um erro ao salvar a categoria")
+         res.redirect("/admin/categorias")
+      })
+
+   }).catch((err) => {
+      req.flash("error_msg", "Houve um erro ao editar a categoria")
+      res.redirect("/admin/categorias")
+   })
 })
 
 module.exports = router
