5269619bfd7d9389749b02a112fcf03d0533cbf4
Guilherme Américo Schiavon <guilherme.schiavon@outlook.com>
2022-06-05 11:56:13 -0300

    Correção da Conexão

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://DevSchiavon:<password>@cluster0.rkubm.mongodb.net/?retryWrites=true&w=majorityt"}
+    module.exports = {mongoURI: "mongodb+srv://DevSchiavon:CBUIv4pTqTazyjrf@cluster0.rkubm.mongodb.net/?retryWrites=true&w=majority"}
    /* console.log("Servidor Produção rodando na porta: " + PORT) */
 } else {
     module.exports = {mongoURI: "mongodb://localhost/blogapp"}
